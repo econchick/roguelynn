a001818b80028de51a86787a4cef9cb128e6e052
Lynn Root <lynn@lynnroot.com>
2017-06-17 12:07:14 -0400

    Fix prism url and redirects to it

--- _deploy.sh
+++ _deploy.sh
@@ -1,1 +1,6 @@
+# -r for recursive
+# -u for skipping files if dest file is newer than source file
+# -p for setting dest permissions same as source
+# -a for archive/preserve everything
+# -z for compression
 mynt gen -f _site && rsync -rupaz _site/ u52704@roguelynn.com:www/
